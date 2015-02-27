c227840eb5394a2de9559f373075ee56679091e8
Michael Dion <mwdion@gmail.com>
2015-02-27 10:19:31 -0500

    Added console log error to add wine function.

--- app/controllers/new.js
+++ app/controllers/new.js
@@ -1,7 +1,11 @@
 app.controller('NewCtrl', function($scope, $state, $db){
   $scope.add = function(wine){
     //add wine to inventory
-    $db.post(wine);
+    $db.post(wine, function(err, res){
+      if(err) { console.log(err);}
+      wine._id = res.id;
+      wine._rev = res.rev;
+    });
     //redirect back to main
     $state.go('main')
     };
